1d054ff3b0c84377a81c0875bce04cb495d61ac3
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2022-12-02 08:15:45 -0300

    Reactor: se apalicado, esta commite irá fazer um update no package.json

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,18 @@ import express from 'express';
 
 import cors from 'cors';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
 
-app.listen(3003,()=>{
-    console.log('Server is running in http://localhost:3003');
+app.listen(process.env.PORT || 3003,()=>{
+    console.log(`Server is running in http://localhost:${process.env.PORT || "3003"}`);
 })
 
 export default app;
